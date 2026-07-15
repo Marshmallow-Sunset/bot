68542c5c5706bd56a6df0077fe9e39f9eccd6a04
Роман <malukin2011@gmail.com>
2026-07-15 17:21:32 +0300

    updated configuration.bat

--- bot-sources/configuration.bat
+++ bot-sources/configuration.bat
@@ -5,6 +5,7 @@ echo Привет.
 echo Эта командная строка позволит вам настроить бота, и приготовить его к исполнению.
 echo .
 set /p TOKEN="your token: "
+mkdir data
 (
 echo({
 echo(    "token": "%TOKEN%"
